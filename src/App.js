bbf3c6cd978f1afa1b943f37964eb05092d3744f
jeffamady <jeff02.jn@gmail.com>
2021-04-28 13:27:10 -0300

    I added some states: contacts and appointments, and I used 2 callback functions to use the setContacts and setAppointments

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
 
 function App() {
-  /*
-  Define state variables for 
-  contacts and appointments 
-  */
-
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
+ 
   const ROUTES = {
     CONTACTS: "/contacts",
     APPOINTMENTS: "/appointments",
   };
 
-  /*
-  Implement functions to add data to
-  contacts and appointments
-  */
+ const addContact = (name, phone, email) => {
+   setContacts((prev) => ([...prev, {name:name, phone: phone, email: email}]));
+ }
+
+ const addAppointment = (title, contact, date, time) => {
+  setAppointments((prev) => ([...prev, {title: title, contact: contact, date: date, time: time}]));
+}
 
   return (
     <>
@@ -36,12 +37,10 @@ function App() {
             <Redirect to={ROUTES.CONTACTS} />
           </Route>
           <Route path={ROUTES.CONTACTS}>
-             {/* Add props to ContactsPage */}
-            <ContactsPage />
+            <ContactsPage contacts={contacts} addContact={addContact} />
           </Route>
           <Route path={ROUTES.APPOINTMENTS}>
-            {/* Add props to AppointmentsPage */}
-            <AppointmentsPage />
+            <AppointmentsPage contacts={contacts} appointments={appointments} addAppointment={addAppointment} />
           </Route>
         </Switch>
       </main>
